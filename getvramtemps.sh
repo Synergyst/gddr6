60ebdd0f7e56d544a342113fa5a294400a9df8cd
Synergyst <coletonm97@gmail.com>
2023-09-24 11:00:41 -0500

    Fix BASH version

--- getvramtemps.sh
+++ getvramtemps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for m in `lspci -v|grep -A10 VGA|grep -A10 NVIDIA|egrep -v 'Subsystem:|Flags: |I/O ports at |Expansion ROM at |Capabilities: '|grep -A1 'VGA compatible controller: '|grep 'Memory at '|awk '{print $3}'` ; do
+# Usage: ./getvramtemps.sh <PCI slot ID>
+# ie: ./getvramtemps.sh 22:00.0
+for m in $(lspci -vs 0000:$1|grep 'Memory at '|awk '{print $3}'|head -n1) ; do
   base_address=0x$m
   memory_offset=0x9a44b0
   result=$(printf "0x%X" "$((base_address + memory_offset))")
